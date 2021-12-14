774c686139f51c67ff7d1258dec0c06112766bff
Сергей <040620@mail.ru>
2021-12-14 20:39:57 +0300

    Homework2.7 check

--- src/main/java/pro/sky/lesson2_7/exception/DuplicateEmployeeException.java
+++ src/main/java/pro/sky/lesson2_7/exception/DuplicateEmployeeException.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
 public class DuplicateEmployeeException extends RuntimeException {
     public DuplicateEmployeeException(String s) {
+        super(s);
         System.out.println(s);
     }
 }
