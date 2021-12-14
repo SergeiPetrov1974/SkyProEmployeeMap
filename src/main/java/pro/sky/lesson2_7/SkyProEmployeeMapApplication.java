7acc9e568bbfbba669b07844a5bf9e91401472ca
Сергей <040620@mail.ru>
2021-12-14 13:23:22 +0300

    Initial commit

---
+++ src/main/java/pro/sky/lesson2_7/SkyProEmployeeMapApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.lesson2_7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyProEmployeeMapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkyProEmployeeMapApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/lesson2_7/SkyProEmployeeMapApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.lesson2_7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyProEmployeeMapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
